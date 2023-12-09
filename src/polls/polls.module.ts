1e04767337735288d1e6f5bc912f44e6f9f4858c
Mohamed Sherif <79464179+mohamedsherif95@users.noreply.github.com>
2023-12-09 13:37:09 +0200

    deploying and connection to redis

--- src/polls/polls.module.ts
+++ src/polls/polls.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PollsService } from './polls.service';
 import { PollsController } from './polls.controller';
+import { redisModule } from '../modules.config';
+
 
 @Module({
+  imports: [redisModule],
   controllers: [PollsController],
   providers: [PollsService]
 })
