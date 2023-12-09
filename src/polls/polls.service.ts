bd449b1943dcadb23bf6f78033173d2af03d3195
Mohamed Sherif <79464179+mohamedsherif95@users.noreply.github.com>
2023-12-09 03:18:22 +0200

    adding polls service, types and id generators

--- src/polls/polls.service.ts
+++ src/polls/polls.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { CreatePollFields, JoinPollFields, RejoinPollFields } from './types';
+import { createPollID, createUserID } from './ids';
 
 @Injectable()
-export class PollsService {}
+export class PollsService {
+  async createPoll(fields: CreatePollFields) {
+    const pollID = createPollID();
+    const userID = createUserID();
+
+    return {
+      ...fields,
+      pollID,
+      userID
+    }
+  }
+
+  async joinPoll(fields: JoinPollFields) {
+    const userID = createUserID();
+
+    return {
+      ...fields,
+      userID
+    }
+  }
+
+  async rejoinPoll(fields: RejoinPollFields) {
+    return {
+      ...fields
+    }
+  }
+}
